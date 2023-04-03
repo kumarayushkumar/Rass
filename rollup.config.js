d334b82093ede931fc2d8ca3949196e205c4841b
Ayush kumar <kumarayushkumar@protonmail.com>
2023-04-03 21:13:27 +0530

    eslint

--- rollup.config.js
+++ rollup.config.js
@@ -1,11 +1,11 @@
-import resolve from '@rollup/plugin-node-resolve';
-import commonjs from '@rollup/plugin-commonjs';
-import typescript from '@rollup/plugin-typescript';
-import dts from 'rollup-plugin-dts';
-import postcss from 'rollup-plugin-postcss';
-import scss from 'rollup-plugin-scss';
+import resolve from '@rollup/plugin-node-resolve'
+import commonjs from '@rollup/plugin-commonjs'
+import typescript from '@rollup/plugin-typescript'
+import dts from 'rollup-plugin-dts'
+import postcss from 'rollup-plugin-postcss'
+import scss from 'rollup-plugin-scss'
 
-import packageJson from './package.json' assert { type: 'json' };
+import packageJson from './package.json'
 
 export default [
   {
@@ -14,26 +14,26 @@ export default [
       {
         file: packageJson.main,
         format: 'cjs',
-        sourcemap: true,
+        sourcemap: true
       },
       {
         file: packageJson.module,
         format: 'esm',
-        sourcemap: true,
-      },
+        sourcemap: true
+      }
     ],
     plugins: [
       resolve(),
       commonjs(),
       typescript({ tsconfig: './tsconfig.json' }),
       postcss(),
-      // scss()
-    ],
+      scss()
+    ]
   },
   {
     input: 'dist/esm/types/index.d.ts',
     output: [{ file: 'dist/index.d.ts', format: 'esm' }],
     plugins: [dts()],
-    external: [/\.(css|scss)$/],
-  },
-];
+    external: [/\.(css|scss)$/]
+  }
+]
